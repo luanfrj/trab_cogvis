a4be4a8bc3dfdff7acf7fa75c7de235887744efe
Luan Ferreira Reis de Jesus <luan@luan.heliohost.org>
2016-06-17 13:38:35 -0300

    Adicionado novo modelo de rede nin-imagenet

--- treinar.sh
+++ treinar.sh
@@ -6,7 +6,7 @@ TOOLS=/home/luan/cognicaovisual/caffe/build/tools
 
 # o parametro -weights que determina que a rede usará os pesos 
 # da rede pré treinada
-$TOOLS/caffe train -solver $MODEL/solver.prototxt -weights $MODEL/bvlc_reference_caffenet.caffemodel
+$TOOLS/caffe train -solver $MODEL/solver.prototxt -weights $MODEL/nin_imagenet_conv.caffemodel
 
 # Para treinar usando a GPU comente a linha de cima e descomente a próxima.
 # $TOOLS/caffe train -solver $MODEL/solver.prototxt -weights $MODEL/bvlc_reference_caffenet.caffemodel -gpu 0
